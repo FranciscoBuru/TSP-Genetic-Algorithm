ba5660d08191de7ea565cee8666550329f61e2c6
franciscoburu <francisco_aramburu@hotmail.com>
2021-12-06 17:56:25 -0600

    Inicial no tan inicial

---
+++ Geneticos.jl
@@ -0,0 +1,202 @@
+using LinearAlgebra
+using Random
+using StatsBase
+using Distributions
+
+abstract type Genoma end
+abstract type GenomaEntero<:Genoma end
+abstract type GenomaBinario<:Genoma end
+abstract type GenomaEntero2Hijos<:GenomaEntero end
+abstract type GenomaEntero1Hijo<:GenomaEntero end
+
+function corregir!(x::Array{Int64}, y::Array{Int64})
+    n=length(x)
+    com=union(x,y)
+    difx=setdiff(com, x)
+    if isempty(difx)
+        return
+    end
+    dify=setdiff(com, y)
+    for i in 1:length(difx)
+        x[findlast(isequal(dify[i]),x)]=difx[i]
+        y[findlast(isequal(difx[i]),y)]=dify[i]
+    end
+end
+
+function mutar!(x::Array{Int64}, Pm::Float64)
+    n=length(x)
+    n_mutaciones=rand(Binomial(n,Pm))
+    for i in 1:n_mutaciones
+        j=sample(Vector(1:n), 2; replace=false)
+        x[j[1]],x[j[2]]=x[j[2]],x[j[1]]
+    end
+end
+
+mutable struct PMX<:GenomaEntero2Hijos
+    genoma::Array{Int64}
+    calif::Int64
+
+    function PMX(starter::Int64, ender::Int64, rng::MersenneTwister, funCalif::Function)
+        genoma=shuffle(rng, Vector(starter:ender))
+        calif=funCalif(genoma)
+        new(genoma, calif)
+    end
+    function PMX(genoma::Array{Int64}, funCalif::Function)
+        new(genoma, funCalif(genoma))
+    end
+end
+
+function crossover(p1::PMX, p2::PMX, rng::MersenneTwister, funCalif::Function, Pm::Float64)
+    n=length(p1.genoma)
+    i=sample(Vector(1:n), 2; replace=false)
+    x=copy(p1.genoma); y=copy(p2.genoma)
+    if i[1]<i[2]
+        i[1],i[2]=i[2],i[1]
+    end
+    y[1:i[1]],x[1:i[1]]=x[1:i[1]],y[1:i[1]]
+    y[i[2]:n],x[i[2]:n]=x[i[2]:n],y[i[2]:n]
+    #corregir las cadenas
+    corregir!(x,y)
+    mutar!(x, Pm); mutar!(y, Pm)
+    return PMX(x,funCalif), PMX(y,funCalif)
+end
+
+mutable struct OX<:GenomaEntero2Hijos
+    genoma::Array{Int64}
+    calif::Int64
+    function OX(starter::Int64, ender::Int64, rng::MersenneTwister, funCalif::Function)
+        genoma=shuffle(rng, Vector(starter:ender))
+        calif=funCalif(genoma)
+        new(genoma, calif)
+    end
+    function OX(genoma::Array{Int64}, funCalif::Function)
+        new(genoma, funCalif(genoma))
+    end
+end
+
+function crossover(p1::OX, p2::OX, rng::MersenneTwister, funCalif::Function, Pm::Float64)
+    n=length(p1.genoma)
+    x=copy(p1.genoma); y=copy(p2.genoma)
+    i=sample(Vector(1:n), 2; replace=false)
+    if i[1]<i[2]
+        j=[1:i[1];i[2]:n]
+    else
+        j=[1:i[2];i[1]:n]
+    end
+
+    #Extraemos los elementos que cambiamos
+    xj=x[j]; yj=y[j]
+    #Obtenemos su nuevo orden
+    sort!(xj, by=i->findfirst(isequal(i),y))
+    sort!(yj, by=i->findfirst(isequal(i),x))
+    #Hacemos el cambio
+    x[j]=xj
+    y[j]=yj
+    #Mutamos
+    mutar!(x, Pm); mutar!(y, Pm)
+    return OX(x,funCalif), OX(y,funCalif)
+end
+
+
+mutable struct Poblacion{T<:Genoma}
+    pob::Array{T} #Poblacion en si
+    rng::MersenneTwister #Seed
+    funCalif::Function #Funcion calificadora, debe ser positiva entera y a maximizar
+    keepbest::Bool #Numero de genomas que se guardan de la poblacion pasada
+    random::Int64 #Numero de genomas que se inicializan al azar cada vuelta
+    Pm::Float64 #Probabilidad de mutacion
+    #Constructor entero
+    function Poblacion{T}(n::Int64, starter::Int64, ending::Int64, funCalif::Function; keepbest::Bool=true, random::Int64=2, Pm::Float64=0.05, seed::Int64=1234) where T<:GenomaEntero
+        rng=MersenneTwister(seed)
+        pob=Array{T}(undef, n)
+        for i in 1:n
+            pob[i]=T(starter, ending, rng, funCalif)
+        end
+        new{T}(pob, rng, funCalif, keepbest, random, Pm)
+    end
+    #Constructor binario
+    function Poblacion{T}(n::Int64, len::Int64, funCalif::Function; keepbest::Bool=true, random::Int64=2, Pm::Float64=0.05, seed::Int64=1234) where T<:GenomaBinario
+        rng=MersenneTwister(seed)
+        pob=Array{T}(undef, n)
+        for i in 1:n
+            pob[i]=T(len, funCalif)
+        end
+        new{T}(pob, rng, funCalif, keepbest, random, Pm)
+    end
+    #Constructor default
+    function Poblacion{T}(pob::Array{T}, rng::MersenneTwister, funCalif::Function, keepbest::Bool, random::Int64, Pm::Float64) where T<:Genoma
+        new(pob, rng, funCalif, keepbest, random, Pm)
+    end
+end
+
+function rouletteselector(pob::Array{T}, n::Int64) where T<:Genoma
+    f=x->x.calif; w=f.(pob)
+    W=FrequencyWeights(-w.+(maximum(w)+1))
+    sample(pob, W, n; replace=false)
+end
+
+function reproduce(p::Poblacion{T}) where T<:GenomaEntero2Hijos
+    n=length(p.pob)
+    pob=Array{T}(undef, n)
+    m=n-p.keepbest-p.random
+    for i in 1:ceil(Int,m/2)
+        s=rouletteselector(p.pob, 2)
+        c=crossover(s[1], s[2], p.rng, p.funCalif, p.Pm)
+        #(p1::OX, p2::OX, rng::MersenneTwister, funCalif::Function, Pm::Float64)
+        pob[2*i-1]=c[1]
+        if 2*i<m
+            pob[2*i]=c[2]
+        end
+    end
+    if p.random>0
+        starter=minimum(pob[1].genoma)
+        ender=maximum(pob[1].genoma)
+        for i in 1:p.random
+            pob[m+i]=T(starter, ender, p.rng, p.funCalif)
+        end
+    end
+    if p.keepbest
+        f=x->x.calif
+        pob[n]=p.pob[findfirst(isequal(minimum(f,p.pob)),f.(p.pob))]
+    end
+    Poblacion{T}(pob, p.rng, p.funCalif, p.keepbest, p.random, p.Pm)
+end
+function getbest(pob::Poblacion)
+    f=x->x.calif;
+    return pob.pob[findfirst(isequal(minimum(f,pob.pob)),f.(pob.pob))]
+end
+
+function calif(pnt)
+    n = length(pnt)
+    #print(n)
+    cost = 0;
+    for p in 1:n-1
+        cost += _mtz[pnt[p], pnt[p+1]]
+    end
+    cost += _mtz[pnt[1], pnt[n]]
+end
+#p=Poblacion{PMX}(10, 1, 6, calif; seed=1)
+
+
+function algoritmoGenetico(funCalif::Function, tipo, pobsize, generations; Pm=0.01, intStart=0, intEnd=0, binlen=10, seed=1234, keepbest=true,random=2)
+    if tipo=="PMX"
+        pob=Poblacion{PMX}(pobsize, intStart, intEnd, funCalif)#; keepbest=keepbest, random=random, Pm=Pm, seed=seed)
+        #Poblacion{T}(n::Int64, starter::Int64, ending::Int64, funCalif::Function; keepbest::Bool=true, random::Int64=2, Pm::Float64=0.05, seed::Int64=1234)
+    elseif tipo=="OX"
+        pob=Poblacion{OX}(pobsize, intStart, intEnd, funCalif; keepbest=keepbest, random=random, Pm=Pm, seed=seed)
+    end
+
+    for i in 2:generations
+        println(i,getbest(pob))
+        pob=reproduce(pob)
+    end
+    res=getbest(pob)
+    return res.calif, res.genoma
+end
+
+## Lee Datos
+
+data = datos("Data1.txt")
+global const _mtz = data[1]
+length(rutaInicial)
+algoritmoGenetico(calif, "OX", 20, length(rutaInicial); intStart=1, intEnd=131)
---
+++ txtFormat.jl
@@ -0,0 +1,66 @@
+using DelimitedFiles
+using LinearAlgebra
+using Random
+using StatsBase
+using Distributions
+
+struct datos
+    dist::Array{Int64}
+    startPoint::Array{Int64}
+
+    function datos(name::String)
+        dist, startPoint = readData(name);
+    end
+
+    function start(num)
+        return shuffle(MersenneTwister(1234), Vector(1:num))
+    end
+
+    function readData(name)
+        # Saco arreglo de posiciones
+        #   ini = time_ns()
+        file = open(name);
+        lines = parse(Int64,readline(file, keep=false))
+        startPoint = start(lines)
+        pos  = zeros(Int64, lines, 2)
+        i = 1
+        while eof(file) != true
+            x,y = split(readline(file, keep=false), " ")[2:3]
+            x = parse(Int64, x)
+            y = parse(Int64, y)
+            pos[i,1] = x
+            pos[i,2] = y
+            i = i+1
+        end
+        # Creo mtz de distancias
+        dist = zeros(Int64, lines, lines)
+        for j in 1:lines
+            for jj in 1:lines
+                dist[j,jj] = round(sqrt((pos[j,1]-pos[jj,1])^2 + (pos[j,2]-pos[jj,2])^2))
+            end
+        end
+        # print("File loaded in ")
+        # print((time_ns() - ini )/ 1e9)
+        # print(" sec")
+        return dist, startPoint
+    end
+end
+
+
+function costoRuta(dist, pnt)
+    n = length(pnt)
+    cost = 0;
+    for p in 1:n-1
+        cost += dist[pnt[p], pnt[p+1]]
+    end
+    cost += dist[pnt[1], pnt[n]]
+end
+
+# Construimos clase con archivo de datos 1
+data = datos("Data1.txt")
+# Sacamos la mtz de costos y la ruta inicial
+matrizDistancias, rutaInicial = data
+
+# Verificamos costos de ruta
+costoRuta(data[1],data[2])
+costoRuta(matrizDistancias,rutaInicial)
