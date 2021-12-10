fa504d99f7fa4c8288154e724eea46f865fa4b4c
franciscoburu <francisco_aramburu@hotmail.com>
2021-12-10 16:01:30 -0600

    CX

--- main.jl
+++ main.jl
@@ -7,8 +7,15 @@ mtz=data.dist
 
 calif=x->costoRuta(x, mtz)
 
-califFinal, genomaFinal = algoritmoGenetico(calif, "OX", 40, 1000; intStart=1, intEnd=floor(Int,length(data.startPoint)/2), random=5)
+califFinal, genomaFinal = algoritmoGenetico(calif, "OX", 100, 1000000; intStart=1, intEnd=floor(Int,length(data.startPoint)/2), random=5)
 
 graficar(data.startPoint, genomaFinal, califFinal)
 
 #califFinal, genomaFinal = algoritmoGenetico(calif, "PMX", 40, 10000; intStart=1, intEnd=131)
+
+
+# ini = time_ns()
+
+# print("File loaded in ")
+# print((time_ns() - ini )/ 1e9)
+# print(" sec")
