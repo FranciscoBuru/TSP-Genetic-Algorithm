521853513036e936ec9312021329f355ae2e7e5c
danjinich <dan@jinich.com.mx>
2021-12-10 13:15:57 -0600

    Mejora datos

--- txtFormat.jl
+++ txtFormat.jl
@@ -12,41 +12,41 @@ struct datos
         dist, startPoint = readData(name);
         new(dist, startPoint)
     end
-
-    function start(num)
-        return shuffle(MersenneTwister(1234), Vector(1:num))
-    end
-
-    function readData(name)
-        # Saco arreglo de posiciones
-        #   ini = time_ns()
-        file = open(name);
-        lines = parse(Int64,readline(file, keep=false))
-        startPoint = start(lines)
-        pos  = zeros(Int64, lines, 2)
-        i = 1
-        while eof(file) != true
-            x,y = split(readline(file, keep=false), " ")[2:3]
-            x = parse(Int64, x)
-            y = parse(Int64, y)
-            pos[i,1] = x
-            pos[i,2] = y
-            i = i+1
-        end
-        # Creo mtz de distancias
-        dist = zeros(Int64, lines, lines)
-        for j in 1:lines
-            for jj in 1:lines
-                dist[j,jj] = round(sqrt((pos[j,1]-pos[jj,1])^2 + (pos[j,2]-pos[jj,2])^2))
-            end
-        end
-        # print("File loaded in ")
-        # print((time_ns() - ini )/ 1e9)
-        # print(" sec")
-        return dist, pos
-    end
 end
 
+
+function start(num)
+    return shuffle(MersenneTwister(1234), Vector(1:num))
+end
+
+function readData(name)
+    # Saco arreglo de posiciones
+    #   ini = time_ns()
+    file = open(name);
+    lines = parse(Int64,readline(file, keep=false))
+    startPoint = start(lines)
+    pos  = zeros(Int64, lines, 2)
+    i = 1
+    while eof(file) != true
+        x,y = split(readline(file, keep=false), " ")[2:3]
+        x = parse(Int64, x)
+        y = parse(Int64, y)
+        pos[i,1] = x
+        pos[i,2] = y
+        i = i+1
+    end
+    # Creo mtz de distancias
+    dist = zeros(Int64, lines, lines)
+    for j in 1:lines
+        for jj in 1:lines
+            dist[j,jj] = round(sqrt((pos[j,1]-pos[jj,1])^2 + (pos[j,2]-pos[jj,2])^2))
+        end
+    end
+    # print("File loaded in ")
+    # print((time_ns() - ini )/ 1e9)
+    # print(" sec")
+    return dist, pos
+end
 
 function costoRuta(pnt, mtz)
     n = length(pnt)
