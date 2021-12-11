6a0bf9ecd8c12698241b5de52d9f76c5b478a2fc
danjinich <dan@jinich.com.mx>
2021-12-10 19:45:28 -0600

    .

--- scriptNuevo.jl
+++ scriptNuevo.jl
@@ -1,7 +1,7 @@
 include("Geneticos.jl")
 include("txtFormat.jl")
 
-data=datos("Data1.txt")
+datas=datos("Data1.txt")
 
 Pm_OX=0.1
 pob_OX=5
@@ -16,18 +16,19 @@ calif=x->costoRuta(x, mtz)
 
 tipos=["OX"; "CX"; "PMX"]
 pms=[Pm_OX; Pm_CX; Pm_PMX]
-pobs=[pob_OX; pob_CMX; pob_PMX]
+pobs=[pob_OX; pob_CX; pob_PMX]
 
-generaciones=[1000; 10000; 100000; 1000000]
-
+#generaciones=[1000; 10000; 100000; 1000000]
+#generaciones=[10]
 graficas=Array{Plots.Plot}(undef, 3)
 graficas2=Array{Plots.Plot}(undef, 3)
 
 for i in 1:3
     for gen in generaciones
         ini = time_ns()
-        p, califFinal, genomaFinal = algoritmoGeneticoReporte(calif, tipos[i], pobs[i], genes; intStart=1, intEnd=size(mtz,1 ), random=0, Pm=pms[i])
-        println(tipo, " Tiempo:"(time_ns() - ini )/ 1e9, ", calif:" califFinal)
+        val, califFinal, genomaFinal, p = algoritmoGeneticoReporte(calif, tipos[i], pobs[i], gen; intStart=1, intEnd=size(mtz,1 ), random=0, Pm=pms[i])
+        println(tipo, ", Tiempo:",(time_ns() - ini )/ 1e9, ", generaciones:",gen, ", calif:", califFinal)
     end
     graficas[i]=graficar(data.startPoint, genomaFinal, califFinal)
+    graficas2[i]=p
 end
